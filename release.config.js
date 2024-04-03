ee1362b4e4bcd810b3ba574ada4618d228f1aeef
AruNiv <163409156+AruNiv@users.noreply.github.com>
2024-04-03 16:55:03 +0530

    Feat: Update release.config.js

--- release.config.js
+++ release.config.js
@@ -3,12 +3,11 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/npm', 
-    ["@semantic-release/github", {
-      "assets": ["*.sh","package.json"]
+    ["@semantic-release/git", {
+      "assets": ["*.sh", "package.json"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    '@semantic-release/git'
+    '@semantic-release/github'
   ]
 };
 
