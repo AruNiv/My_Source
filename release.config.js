7c8c27c52de6d2436a721cb424f08475f9c88a86
AruNiv <163409156+AruNiv@users.noreply.github.com>
2024-04-15 12:14:26 +0530

    fix: removed semantic-release/git release.config.js

--- release.config.js
+++ release.config.js
@@ -3,14 +3,11 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ["@semantic-release/git", {
-      "assets": ["*.sh", "package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
+    
    [
       "@semantic-release/exec",
       {
-        "verifyReleaseCmd": "echo ${nextRelease.version} > .VERSION"
+        "prepareCmd": "./welcome.sh ${nextRelease.version}"
       }
     ],
     '@semantic-release/github'
