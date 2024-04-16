f075c2716c1ea6150e221c7873bf33b79a888382
AruNiv <163409156+AruNiv@users.noreply.github.com>
2024-04-16 14:38:08 +0530

    perf: Update release.config.js
    BREAKING CHANGE

--- release.config.js
+++ release.config.js
@@ -1,21 +1,31 @@
 const config = {
   branches: [ 'main' ],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-     ["@semantic-release/git", {
-      "assets": ["*.sh", "package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],  
+   "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/changelog",
+      {
+          "changelogFile": "CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["package.json", "CHANGELOG.md"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
     [
       "@semantic-release/exec",
       {
         "verifyReleaseCmd": "bash welcome.sh ${nextRelease.version}",
         "generateNotesCmd": "git log -1 --pretty=%B >> release.txt"
       }
-    ],
+    ]
     
-    '@semantic-release/github'
   ]
 };
 
