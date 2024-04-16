a9ed7efd0b2eb8eafe900a6559c95d27c5ca5237
AruNiv <163409156+AruNiv@users.noreply.github.com>
2024-04-16 14:07:47 +0530

    feat: Added the semantic-release/exec

--- release.config.js
+++ release.config.js
@@ -1,12 +1,20 @@
 const config = {
-  branches: ['main'],
+  branches: [ 'main' ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
      ["@semantic-release/git", {
       "assets": ["*.sh", "package.json"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],    
+    }],  
+    [
+      "@semantic-release/exec",
+      {
+        "prepareCmd": "set-version ${nextRelease.version}",
+        "verifyReleaseCmd": "bash welcome.sh ${nextRelease.version}",
+        "generateNotesCmd": "git log -1 --pretty=%B >> release.txt"        
+      }
+    ],
     
     '@semantic-release/github'
   ]
