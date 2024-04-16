efef7078683da4c46c16f2651524c3899c4bc65e
AruNiv <163409156+AruNiv@users.noreply.github.com>
2024-04-16 13:05:36 +0530

    feat: set version added in release.config.js

--- release.config.js
+++ release.config.js
@@ -3,17 +3,19 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      "@semantic-release/exec",
-      {
-        "verifyReleaseCmd": "bash welcome.sh ${nextRelease.version}",
-        "generateNotesCmd": "git log -1 --pretty=%B >> release.txt"
-      }
-    ],
      ["@semantic-release/git", {
       "assets": ["*.sh", "package.json"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
+    [
+      "@semantic-release/exec",
+      {
+        "prepareCmd": "set-version ${nextRelease.version}",
+        "verifyReleaseCmd": "bash welcome.sh ${nextRelease.version}",
+        "generateNotesCmd": "git log -1 --pretty=%B >> release.txt"        
+      }
+    ],
+    
     '@semantic-release/github'
   ]
 };
