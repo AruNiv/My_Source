a657e55b1f8993f41c959a839ded7b84c9f6c4b8
AruNiv <163409156+AruNiv@users.noreply.github.com>
2024-04-24 14:07:49 +0530

    Update find_replace.sh

--- find_replace.sh
+++ find_replace.sh
@@ -3,4 +3,7 @@
 old_version=`grep ^Version < ./My_Target/install/release/new_release.txt | cut -d"=" -f2`
 new_version=`grep ^[0-9] < release.txt`
 
+echo "old_version = $old_version"
+echo "new_version = $new_version"
+
 sed -i 's/$old_version/$new_version/g' ./My_Target/install/release/new_release.txt
