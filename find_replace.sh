78b07c9e9ab58bcdbc4c98d8af4324983e998f64
AruNiv <163409156+AruNiv@users.noreply.github.com>
2024-04-24 14:04:38 +0530

    fix: Update find_replace.sh

--- find_replace.sh
+++ find_replace.sh
@@ -3,4 +3,4 @@
 old_version=`grep ^Version < ./My_Target/install/release/new_release.txt | cut -d"=" -f2`
 new_version=`grep ^[0-9] < release.txt`
 
-sed -i 's/$old_version/$new_version/g' AruNiv/My_Target/install/release/new_release.txt
+sed -i 's/$old_version/$new_version/g' ./My_Target/install/release/new_release.txt
